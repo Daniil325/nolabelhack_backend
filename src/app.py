8c1513eb168f0144db3f9d568d2efd8d46cddc05
Daniil325 <potekhindaniil04@gmail.com>
2025-02-08 00:27:02 +0700

    feat: app middlewares to app

--- src/app.py
+++ src/app.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from src.presentation.routers import api_router
 from dishka.integrations.fastapi import setup_dishka
+from starlette.middleware.cors import CORSMiddleware
 from src.infra.di import container
 
 
 def create_app():
     app: FastAPI = FastAPI()
     app.include_router(api_router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['http://localhost:3000'],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     setup_dishka(container, app)
 
